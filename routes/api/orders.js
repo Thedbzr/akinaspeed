20f7a5916c1cd406a9c2f7c1e2edaf4d69bd4fc5
Anthony Martinez <martinezanthony213@gmail.com>
2021-01-29 16:11:21 -0800

    completes Routes

--- routes/api/orders.js
+++ routes/api/orders.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const router = express.Router();
-const itemsCtrl = require('../../controllers/api/items');
+const ordersCtrl = require('../../controllers/api/orders');
 
-// GET /api/items
-router.get('/', itemsCtrl.index);
-// GET /api/items/:id
-router.get('/:id', itemsCtrl.show);
+// GET /api/orders/cart
+router.get('/cart', ordersCtrl.cart);
+// POST /api/orders/cart/items/:id
+router.post('/cart/items/:id', ordersCtrl.addToCart);
+// POST /api/orders/cart/checkout
+router.post('/cart/checkout', ordersCtrl.checkout);
+// POST /api/orders/cart/qty
+router.put('/cart/qty', ordersCtrl.setItemQtyInCart);
 
 module.exports = router;
