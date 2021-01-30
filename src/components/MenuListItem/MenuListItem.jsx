3acf1bd51bef1a31949d8add4cb1aa8a4bb83eff
Anthony Martinez <martinezanthony213@gmail.com>
2021-01-30 15:44:30 -0800

    styles item grid

--- src/components/MenuListItem/MenuListItem.jsx
+++ src/components/MenuListItem/MenuListItem.jsx
@@ -14,29 +14,20 @@ export default function MenuListItem({ menuItem, handleAddToOrder }) {
         </div>
       </div> */}
       <div>
-        <div className="card">
+        <div className="card h-100">
           <div className="card-image">
             <figure className="image is-4by3">
               <img src={menuItem.url} alt="really cool product image" />
             </figure>
           </div>
           <div className="card-content">
-            <p>{menuItem.brand.toUpperCase()}</p>
-            <p>{menuItem.name.toUpperCase()}</p>
+            <p className="itemName">{menuItem.name.toUpperCase()}</p>
           </div>
-          <footer className="card-footer">
-            <p className="card-footer-item">
-              <span>
-                Price: {menuItem.price}
-              </span>
-            </p>
-            <p className="card-footer-item">
-              <span>
-                <button className="button is-small" onClick={() => handleAddToOrder(menuItem._id)}>
-                  ADD
-        </button>
-              </span>
-            </p>
+          <hr className="itemHr" />
+          <footer className="cardfooter">
+            <span>Price: ${menuItem.price}</span>
+            &nbsp; | &nbsp;
+            <span><button className="button is-small" onClick={() => handleAddToOrder(menuItem._id)}>ADD</button></span>
           </footer>
         </div>
       </div>
